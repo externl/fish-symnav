03e3f08bfd57e11c33384a9594e47aa2ca26368e
Joe George <joe@externl.com>
2017-09-01 16:23:37 -0400

    Fix completions for abs paths with symlink

--- functions/__symdir_complete.fish
+++ functions/__symdir_complete.fish
@@ -6,29 +6,36 @@
 function __symdir_complete
     # Setup variables and install shadow functions
     __symdir_initialize
-    if not __symdir_is_pwd
-        set -l token (commandline --current-token)
-        set -l relative_path (__symdir_relative_to "$token")
-        if __symdir_is_absolute $relative_path
-            switch $symdir_substitution_mode
-                case "ask"
-                    printf "$relative_path\n"(dirname $PWD) | fzf | read -l selection
-                    commandline -f repaint
-                    if test $selection = $relative_path
-                        set -l commandline_list (commandline --tokenize)[1..-2] "$relative_path"
-                        commandline --replace (string join ' ' $commandline_list)
-                    else
-                        #XXX Maybe this should just leave the path alone?
-                        set -l commandline_list (commandline --tokenize)[1..-2] (dirname $PWD)
-                        commandline --replace (string join ' ' $commandline_list)
-                    end
-                case "symlink"
+    set -l current_buffer (commandline --current-buffer)
+    set -l token (commandline --current-token)
+    set -l relative_path (__symdir_relative_to "$token")
+
+    # If the "relative path" returned by __symdir_relative_to is actually an absolute path then
+    # we know it encountered a symbolic link during path resolution so we should try to substitute
+    # the for current token
+    if __symdir_is_absolute $relative_path
+        switch $symdir_substitution_mode
+            case "ask"
+                printf "$relative_path\n"(dirname $PWD) | fzf | read -l selection
+                commandline -f repaint
+                if test $selection = $relative_path
                     set -l commandline_list (commandline --tokenize)[1..-2] "$relative_path"
                     commandline --replace (string join ' ' $commandline_list)
-                case *
-                    echo "$symdir_substitution_mode is not a valid option for \$symdir_substitution_mode" 1>&2
-            end
+                else
+                    #XXX Maybe this should just leave the path alone?
+                    set -l commandline_list (commandline --tokenize)[1..-2] (dirname $PWD)
+                    commandline --replace (string join ' ' $commandline_list)
+                end
+            case "symlink"
+                set -l commandline_list (commandline --tokenize)[1..-2] "$relative_path"
+                commandline --replace (string join ' ' $commandline_list)
+            case *
+                echo "$symdir_substitution_mode is not a valid option for \$symdir_substitution_mode" 1>&2
         end
+
+        test $symdir_execute_substitution -eq 0
+            and test $current_buffer != (commandline --current-buffer)
+            and return
     end
     commandline -f complete
 end
